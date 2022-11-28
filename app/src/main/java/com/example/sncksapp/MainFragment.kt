81f836f209bff1ca4a997239239a0cce6bee9529
Andraž Valentincic <andraz.valentincic@student.um.si>
2022-11-28 11:12:47 +0100

    add OpenStreetMap

--- app/src/main/java/com/example/sncksapp/MainFragment.kt
+++ app/src/main/java/com/example/sncksapp/MainFragment.kt
@@ -1,11 +1,15 @@
 package com.example.sncksapp
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat.requestPermissions
 import com.example.sncksapp.databinding.FragmentMainBinding
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.views.MapView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,8 +24,9 @@ private const val ARG_PARAM2 = "param2"
 class MainFragment : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
-
     private val binding get() = _binding!!
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
+    private lateinit var map : MapView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,5 +38,21 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        map = _binding!!.mapview
+        map.setTileSource(TileSourceFactory.MAPNIK)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        val permissionsToRequest = ArrayList<String>()
+        var i = 0
+        while (i < grantResults.size) {
+            permissionsToRequest.add(permissions[i])
+            i++
+        }
+        if (permissionsToRequest.size > 0) {
+            requestPermissions(
+                permissionsToRequest.toTypedArray(),
+                REQUEST_PERMISSIONS_REQUEST_CODE)
+        }
     }
 }
