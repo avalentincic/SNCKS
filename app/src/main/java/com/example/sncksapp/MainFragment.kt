32710718c198991d072624560c29a748eb79d62a
Andraž Valentincic <andraz.valentincic@student.um.si>
2022-12-06 16:45:14 +0100

    add - basic OSM markers

--- app/src/main/java/com/example/sncksapp/MainFragment.kt
+++ app/src/main/java/com/example/sncksapp/MainFragment.kt
@@ -5,11 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.lib.VendingMachine
 import com.example.sncksapp.databinding.FragmentMainBinding
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
 
 
 private const val ARG_PARAM1 = "param1"
@@ -19,6 +21,8 @@ class MainFragment : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
+    private lateinit var app: MyApplication
+
     private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
     private lateinit var map : MapView
 
@@ -29,12 +33,17 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
+        app = activity?.application as MyApplication
         map = binding.mapview
         map.setTileSource(TileSourceFactory.MAPNIK)
+
         val mapController = map.controller
         mapController.setZoom(18.0)
         val startPoint = GeoPoint(46.559113978089606, 15.639132629280839)
         mapController.setCenter(startPoint)
+
+        handleMarkers(app.vendingMachines, map)
+
         return binding.root
     }
 
@@ -61,6 +70,33 @@ class MainFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
-        //map.onResume()
+        map.onResume()
     }
 }
+
+fun handleMarkers(items: ArrayList<VendingMachine>, m:MapView){
+    val markers: MutableList<Marker> = ArrayList()
+
+    for (vm in items) {
+
+        val markerLocation = GeoPoint(vm.latitude, vm.longitude)
+
+        val marker = Marker(m)
+        marker.position = markerLocation
+        marker.title = vm.name
+        //marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+
+        markers.add(marker)
+    }
+
+    m.overlays.addAll(markers)
+    m.invalidate()
+}
+
+/*
+var marker = Marker(map)
+marker.position = startPoint
+//marker.icon = ContextCompat.getDrawable(requireContext(), R.drawable.vending_machine)
+//marker.setAnchor(Marker.ANCHOR_BOTTOM, Marker.ANCHOR_CENTER)
+map.overlays.add(marker)
+map.invalidate()*/
